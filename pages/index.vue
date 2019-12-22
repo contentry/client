c3e44e5d2d8369f2996167fcf7e7a06845a7a6a4
Fir3st <fojtik.v@gmail.com>
2019-12-22 12:06:14 +0100

    Switched to TS and configured environment for that

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div>
-            <logo/>
+            <logo />
             <h1 class="title">
                 client
             </h1>
@@ -28,45 +28,48 @@
     </div>
 </template>
 
-<script>
-  import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Logo from '~/components/Logo.vue';
 
-  export default {
+@Component({
     components: {
-      Logo
+        Logo
     }
-  }
+})
+export default class Homepage extends Vue {}
 </script>
 
 <style>
-    .container {
-        margin: 0 auto;
-        min-height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-    }
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
 
-    .title {
-        font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-        display: block;
-        font-weight: 300;
-        font-size: 100px;
-        color: #35495e;
-        letter-spacing: 1px;
-    }
+.title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+        BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+        sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+}
 
-    .subtitle {
-        font-weight: 300;
-        font-size: 42px;
-        color: #526488;
-        word-spacing: 5px;
-        padding-bottom: 15px;
-    }
+.subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+}
 
-    .links {
-        padding-top: 15px;
-    }
+.links {
+    padding-top: 15px;
+}
 </style>
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    "plugins": [
-        "stylelint-scss"
-    ],
-    "rules": {
-        "scss/dollar-variable-pattern": "^foo",
-        "scss/selector-no-redundant-nesting-selector": true
+    plugins: ['stylelint-scss'],
+    rules: {
+        'scss/dollar-variable-pattern': '^foo',
+        'scss/selector-no-redundant-nesting-selector': true
     }
-}
+};
